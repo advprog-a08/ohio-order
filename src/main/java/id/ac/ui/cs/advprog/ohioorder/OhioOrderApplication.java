9bc5cc5dd5d075c2e6b4daca6dabdbb453e02f16
secona <77039267+secona@users.noreply.github.com>
2025-03-28 18:47:48 +0700

    feat: initialize hello world Spring boot app

---
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/OhioOrderApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.ohioorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhioOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhioOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/ohioorder/OhioOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.ohioorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhioOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
