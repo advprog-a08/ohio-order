efc7937e9f43df3fa5efc1624a178ac0ec2897b7
Scallss <pascalhafidz2005@gmail.com>
2025-04-09 14:19:44 +0700

    build: dependencies for psql, jakarta, testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,16 @@ repositories {
     mavenCentral()
 }
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
@@ -33,6 +41,16 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
 }
 
 tasks.test {
--- src/main/java/id/ac/ui/cs/advprog/ohioorder/OhioOrderApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/OhioOrderApplication.java
@@ -2,11 +2,18 @@ package id.ac.ui.cs.advprog.ohioorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class OhioOrderApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+
         SpringApplication.run(OhioOrderApplication.class, args);
     }
 
