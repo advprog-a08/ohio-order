02f4789977afd6124fbd2267427c54de2abb82d8
KrisnaPutraP <krisna.putra@ui.ac.id>
2025-04-09 20:05:14 +0700

    feat: Create meja service interface

---
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/meja/service/MejaService.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.ohioorder.meja.service;
+
+import id.ac.ui.cs.advprog.ohioorder.meja.dto.MejaRequest;
+import id.ac.ui.cs.advprog.ohioorder.meja.dto.MejaResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MejaService {
+    MejaResponse createMeja(MejaRequest request);
+    List<MejaResponse> getAllMeja();
+    MejaResponse getMejaById(UUID id);
+    MejaResponse updateMeja(UUID id, MejaRequest request);
+    void deleteMeja(UUID id);
+}
