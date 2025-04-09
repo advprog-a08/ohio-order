aca0a30b58aa3d112971d91fdf7edcca7089e82c
KrisnaPutraP <krisna.putra@ui.ac.id>
2025-04-09 20:35:18 +0700

    feat: Adding additional methods for meja service interface

--- src/main/java/id/ac/ui/cs/advprog/ohioorder/meja/service/MejaService.java
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/meja/service/MejaService.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.ohioorder.meja.service;
 
 import id.ac.ui.cs.advprog.ohioorder.meja.dto.MejaRequest;
 import id.ac.ui.cs.advprog.ohioorder.meja.dto.MejaResponse;
+import id.ac.ui.cs.advprog.ohioorder.meja.enums.MejaStatus;
 
 import java.util.List;
 import java.util.UUID;
@@ -12,4 +13,9 @@ public interface MejaService {
     MejaResponse getMejaById(UUID id);
     MejaResponse updateMeja(UUID id, MejaRequest request);
     void deleteMeja(UUID id);
+
+    MejaResponse getMejaByNomorMeja(String nomorMeja);
+    MejaResponse setMejaStatus(UUID id, MejaStatus status);
+    boolean isMejaAvailable(UUID id);
+    List<MejaResponse> getAvailableMeja();
 }
