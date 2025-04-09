6accd6d7c530a95fce326011470e72e7d729da1c
KrisnaPutraP <krisna.putra@ui.ac.id>
2025-04-09 20:46:40 +0700

    refactor: Moving utils inside meja because it only configurates the meja

---
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/meja/utils/MejaConfig.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.ohioorder.meja.utils;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MejaConfig {
+    private static MejaConfig instance;
+    
+    @Getter
+    private final int maxTableCount = 100; 
+    
+    @Getter
+    private final String tablePrefix = "T"; // Prefix for table numbers
+    
+    private MejaConfig() {
+        // Private constructor to prevent instantiation
+    }
+    
+    public static synchronized MejaConfig getInstance() {
+        if (instance == null) {
+            instance = new MejaConfig();
+        }
+        return instance;
+    }
+    
+    public String generateTableNumber(int number) {
+        return tablePrefix + number;
+    }
+    
+    public boolean isValidTableNumber(String nomorMeja) {
+        // Table number should start with the prefix and be followed by a number
+        if (!nomorMeja.startsWith(tablePrefix)) {
+            return false;
+        }
+        
+        String numberPart = nomorMeja.substring(tablePrefix.length());
+        try {
+            int tableNumber = Integer.parseInt(numberPart);
+            return tableNumber > 0 && tableNumber <= maxTableCount;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
