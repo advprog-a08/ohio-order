250189e8cff24d827a26ee0715972cea6483dabf
Scallss <pascalhafidz2005@gmail.com>
2025-04-11 17:11:29 +0700

    feat: implement order models

--- src/main/java/id/ac/ui/cs/advprog/ohioorder/pesanan/model/OrderItem.java
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/pesanan/model/OrderItem.java
@@ -1,7 +1,23 @@
 package id.ac.ui.cs.advprog.ohioorder.pesanan.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderItem {
+    @Id
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4",  strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "VARCHAR(36)")
     private String id;
 
     private String menuItemId;
@@ -12,5 +28,7 @@ public class OrderItem {
 
     private int quantity;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
     private Order order;
 }
