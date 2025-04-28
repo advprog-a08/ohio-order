00fd855385f79aef28faa6bbdb339b9c4b892398
secona <77039267+secona@users.noreply.github.com>
2025-04-29 01:08:59 +0700

    feat: implement simple CheckoutService to pass test

---
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/checkout/service/CheckoutService.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.ohioorder.checkout.service;
+
+import id.ac.ui.cs.advprog.ohioorder.checkout.model.Checkout;
+
+public interface CheckoutService {
+    Checkout create();
+}
