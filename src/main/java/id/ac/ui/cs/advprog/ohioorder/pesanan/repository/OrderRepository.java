d13ed7170e479a21f04c8a49e4904a6a2f9ef076
Scallss <pascalhafidz2005@gmail.com>
2025-05-08 21:50:00 +0700

    fix: order repository implementation

--- src/main/java/id/ac/ui/cs/advprog/ohioorder/pesanan/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/pesanan/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.ohioorder.pesanan.repository;
 
 import id.ac.ui.cs.advprog.ohioorder.pesanan.model.Order;
-import id.ac.ui.cs.advprog.ohioorder.pesanan.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
-    List<Order> findByUserId(String userId);
     List<Order> findByMejaId(UUID mejaId);
-    List<Order> findByMejaIdAndStatus(UUID mejaId, OrderStatus status);
 }
