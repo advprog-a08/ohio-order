25c1eb4de5f93c4c31bff44dcd475e58e2475d1e
Scallss <pascalhafidz2005@gmail.com>
2025-05-26 23:29:51 +0700

    fix: order reference with orderItem to prevent recursion

--- src/main/java/id/ac/ui/cs/advprog/ohioorder/pesanan/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/pesanan/model/Order.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.ohioorder.pesanan.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,6 +32,7 @@ public class Order {
     private Meja meja;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @Column(name = "locked", nullable = false)
