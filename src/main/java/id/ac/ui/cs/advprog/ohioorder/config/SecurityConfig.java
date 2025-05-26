00d372a8606ed507aee173f1e0d9d5745ab9ed93
secona <77039267+secona@users.noreply.github.com>
2025-05-26 14:41:17 +0700

    fix: csrf disabled

--- src/main/java/id/ac/ui/cs/advprog/ohioorder/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.ohioorder.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,11 +10,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
