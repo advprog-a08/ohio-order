7269f9048523bc454a4f462b82f91772e4f7b81a
secona <77039267+secona@users.noreply.github.com>
2025-05-26 15:33:29 +0700

    feat: remove csrf token

--- src/main/java/id/ac/ui/cs/advprog/ohioorder/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/ohioorder/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.ohioorder.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +11,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
